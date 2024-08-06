cf02f6c1819224a6963bebd9b9bf9856fa5f45ae
Jesse Clemens <crimson.corelio@gmail.com>
2024-08-06 18:15:31 -0400

    Add a displayname property to measures

--- cronometer/foods/measure.py
+++ cronometer/foods/measure.py
@@ -1,14 +1,26 @@
 """
 """
 
-from pydantic_xml import attr
 from pydantic_xml import BaseXmlModel
+from pydantic_xml import attr
 
 
 class Measure(BaseXmlModel, tag="measure"):
     grams: float = attr()
     amount: float = attr()
     description: str = attr(name="name")
+
+    @property
+    def displayName(self) -> str:
+        """
+        The display name for the measure.
+
+        This combines amount (if it is non-zero) with description
+        """
+        if self.amount == 0.0:
+            return self.description
+        return f"{self.amount:.4} {self.description}"
+
 
 
 GRAM = Measure(grams=1.0, amount=1.0, description="g")
